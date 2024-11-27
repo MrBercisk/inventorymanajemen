aad0482f83ae11830bc2ed60a2943913d499d518
Bimo Satrio Putra Pradana <90496447+MrBercisk@users.noreply.github.com>
2024-11-27 23:03:36 +0700

    Update Database.php
    Add : Config Railway

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -26,10 +26,10 @@ class Database extends Config
      */
     public array $default = [
         'DSN'          => '',
-        'hostname'     => 'localhost',
+        'hostname'     => 'mysql.railway.internal',
         'username'     => 'root',
-        'password'     => '',
-        'database'     => 'inventorykantin',
+        'password'     => 'rSLCGmIDTBJrFuDlzvKHIJZcaBvtTZbj',
+        'database'     => 'railway',
         'DBDriver'     => 'MySQLi',
         'DBPrefix'     => '',
         'pConnect'     => false,
